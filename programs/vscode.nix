45789068f6de8e1e673645a63aade4ff3c360847
Aleksander Gondek <gondekaleksander@protonmail.com>
2019-12-23 17:17:08 +0100

    Added Vscode extensions

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -18,10 +18,22 @@ let
         sha256 = "1mc3awybzd2ql1b86inirhsw3j2c7cs0b0nvbjp38jjpq674bmj7";
       }
       {
+        name = "scala";
+        publisher = "scala-lang";
+        version = "0.3.8";
+        sha256 = "17dl10m3ayf57sqgil4mr9fjdm7i8gb5clrs227b768pp2d39ll9";
+      }
+      {
         name = "theme-monokai-pro-vscode";
         publisher = "monokai";
         version = "1.1.14";
         sha256 = "1qp1vldfz5hjpz2ws30705rdp7jspg9qhs17hh0lvs4p5fdr1f4g";
+      }
+      {
+        name = "latex-support";
+        publisher = "torn4dom4n";
+        version = "3.1.0";
+        sha256 = "1h7jk3bwqrzmjssr4m9spwjy3gh5m6miglllh33h818qx590k9s6";
       }
     ];
   };
