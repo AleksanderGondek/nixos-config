e73671be42b4a18e4bb76a2d7c5184cec453572e
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-01-04 17:49:30 +0100

    Include VirtualBox configuration

---
+++ virtualisation/vbox.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.virtualbox.host = {
+    enable = true;
+    enableExtensionPack = false;
+    addNetworkInterface = true;
+    enableHardening = true;
+    headless = false;
+  };
+  users.extraGroups.vboxusers.members = [ "agondek" ];
+}
