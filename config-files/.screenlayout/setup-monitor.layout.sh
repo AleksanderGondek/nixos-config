cfa9f4eb4c50ac56c823d4cd71d7c4b6d20ae48e
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-04-04 14:24:57 +0200

    Fix monitor layout setup for workstations that are not work laptop

--- config-files/.screenlayout/setup-monitor.layout.sh
+++ config-files/.screenlayout/setup-monitor.layout.sh
@@ -5,6 +5,7 @@ set -o errexit
 
 if [ $(hostname) == "TAG009443491811" ]
 then
+  # Basically DELL 5540
   # Only this machine is naughty in terms of resolution shenaningans
   if [ $(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l) -gt 1 ]
   then
@@ -14,6 +15,7 @@ then
     # Single screen
     /run/current-system/sw/bin/sh ~/.screenlayout/work-laptop.layout.sh
   fi
-  /run/current-system/sw/bin/sh ~/.fehbg
-  /run/current-system/sw/bin/sh ~/.config/polybar/run-polybar.sh
 fi
+
+/run/current-system/sw/bin/sh ~/.fehbg
+/run/current-system/sw/bin/sh ~/.config/polybar/run-polybar.sh
