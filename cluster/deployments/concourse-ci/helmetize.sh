00ca01e1c4f7d563139dd7645060d6b4309f2647
Aleksander Gondek <aleksander.gondek@dynatrace.com>
2020-04-20 09:09:48 +0200

    K8s Concourse CI - minor changes in example deployment

--- cluster/deployments/concourse-ci/helmetize.sh
+++ cluster/deployments/concourse-ci/helmetize.sh
@@ -4,5 +4,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+unset KUBECONFIG
+
 helm repo add concourse https://concourse-charts.storage.googleapis.com/
-helm template --name-template "concourse-ci" --values values.yaml concourse/concourse > test-no-changes.yml
+helm template --name-template "concourse-ci" --values values.yaml concourse/concourse > concourse.yaml
