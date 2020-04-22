9fad18e596be0acc275f57155e1d306799101e14
Aleksander Gondek <aleksander.gondek@dynatrace.com>
2020-04-22 16:26:20 +0200

    Switch VM Utility Drone to Nix 20.03

--- vm-utility-drone.nix
+++ vm-utility-drone.nix
@@ -59,11 +59,11 @@ in {
   # Auto upgrade stable channel
   system.autoUpgrade = {
     enable = true;
-    channel = "https://nixos.org/channels/nixos-19.09";
+    channel = "https://nixos.org/channels/nixos-20.03";
     dates = "weekly";
     # Without explicit nixos config location, you are in for a bad times
     flags = [
-      "-I nixos-config=/home/root/nixos-config/vm-utility-drone.nix"
+      "-I nixos-config=/root/nixos-config/vm-utility-drone.nix"
     ];
   };
 }
