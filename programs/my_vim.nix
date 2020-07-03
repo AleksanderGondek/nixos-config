5c3cbb39feaba2213f7556e9a8e19e1887b2bdf0
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-07-03 22:22:16 +0200

    Custom vim - first pass on looks plugins

--- programs/my_vim.nix
+++ programs/my_vim.nix
@@ -3,6 +3,18 @@
 
 let
   customPlugins = {
+    semantic-highlight = pkgs.vimUtils.buildVimPlugin {
+      name = "semantic-highlight-git-2019-09-01";
+      src = pkgs.fetchgit {
+        url = "https://github.com/jaxbot/semantic-highlight.vim.git";
+        rev = "7cf0aabbd0f9cb531b0045ac2148dff1131616de";
+        sha256 = "16dnqrdpxf6322az1rn67ay2a4clqz410xn6zqzr1w2y6x4yly1a";
+      };
+      meta = {
+        homepage = https://github.com/jaxbot/semantic-highlight.vim;
+        maintainers = [ pkgs.stdenv.lib.maintainers.jagajaga ];
+      };
+    };
   };
 in pkgs.vim_configurable.customize {
   name = "vim";
@@ -10,11 +22,19 @@ in pkgs.vim_configurable.customize {
   vimrcConfig.customRC = ''
   set encoding=utf-8  " The encoding displayed.
   set fileencoding=utf-8  " The encoding written to file.
+  
+  let g:molokai_original = 1
+  let g:airline_theme='molokai'
+  colorscheme molokai
  '';
 
   vimrcConfig.vam.knownPlugins = pkgs.vimPlugins // customPlugins;
   vimrcConfig.vam.pluginDictionaries = [{
     names = [
+      "molokai"
+      "semantic-highlight"
+      "vim-airline"
+      "vim-airline-themes"
       "vim-devicons"
       "nerdtree"
     ];
