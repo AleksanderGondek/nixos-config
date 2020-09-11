ed5a09a55cfac9a909f0b760a365930d560e807b
Aleksander Gondek <gondekaleksander@protonmail.com>
2020-09-11 21:12:46 +0200

    User profile - codify mimeapps.list

--- users/agondek/user-profile.nix
+++ users/agondek/user-profile.nix
@@ -199,5 +199,16 @@ in
         theme = "agnoster";
       };
     };
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = ["vivaldi-stable.desktop"];
+        "text/htm" = ["vivaldi-stable.desktop"];
+        "x-scheme-handler/http" = ["vivaldi-stable.desktop"];
+        "x-scheme-handler/https" = ["vivaldi-stable.desktop"];
+        "x-scheme-handler/about" = ["vivaldi-stable.desktop"];
+        "x-scheme-handler/unknown" = ["vivaldi-stable.desktop"];
+      };
+    };
   };
 }
