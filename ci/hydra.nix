8b9076c7315987df544e6889cb6a1e5fcdb8c0ce
Aleksander Gondek <gondekaleksander@protonmail.com>
2021-04-09 18:22:53 +0200

    Add draft of hydra server exposition

--- ci/hydra.nix
+++ ci/hydra.nix
@@ -20,7 +20,35 @@ in
     buildMachinesFiles = [
     ];
   };
-  
+
+  services.nginx = {
+    enable = true;
+
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    virtualHosts."example.com" =  {
+      enableACME = false;
+      forceSSL = false;
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;" +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;"
+          ;
+      };
+    };
+
+  };
+
+  nix.autoOptimiseStore = true;
   nix.trustedUsers = [
     "hydra"
     "hydra-evaluator"
