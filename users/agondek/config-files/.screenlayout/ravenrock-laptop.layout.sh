4f142042d7637db2f65dd56b912eb379dd2bd8a1
Aleksander Gondek <gondekaleksander@protonmail.com>
2021-07-05 23:58:09 +0200

    Ensure ravenrock laptop detects single monitor setup

--- users/agondek/config-files/.screenlayout/ravenrock-laptop.layout.sh
+++ users/agondek/config-files/.screenlayout/ravenrock-laptop.layout.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
+#!/run/current-system/sw/bin/sh
+
+readonly monitor_count=$(xrandr -q | grep ' connected' | wc -l)
+
+if [ "${monitor_count}" == "2" ]; then
+  xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
+else 
+  xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
+fi
