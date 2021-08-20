02ff4e75ad15fbe316537a4b32ba00f6a84ceea5
Aleksander Gondek <gondekaleksander@protonmail.com>
2021-08-20 15:38:00 +0200

    Kubernetes cluster - minor improvements

--- cluster/addons/cluster-admin.nix
+++ cluster/addons/cluster-admin.nix
@@ -5,7 +5,7 @@
     kind = "ServiceAccount";
     apiVersion = "v1";
     metadata = {
-      name = "sa-cluster-admin";
+      name = "cluster-admin-sa";
       namespace = "kube-system";
       labels = {
         # Custom modification, needed to make it work
@@ -18,7 +18,7 @@
     apiVersion="rbac.authorization.k8s.io/v1";
     kind = "ClusterRoleBinding";
     metadata = {
-      name = "sa-cluster-admin-role-binding";
+      name = "cluster-admin-sa-role-binding";
       labels = {
         "addonmanager.kubernetes.io/mode" = "Reconcile";
       };
@@ -31,7 +31,7 @@
     subjects = [
         {
           kind = "ServiceAccount";
-          name = "sa-cluster-admin";
+          name = "cluster-admin-sa";
           namespace = "kube-system";
         }
       ];
--- cluster/k8s-dev-single-node.nix
+++ cluster/k8s-dev-single-node.nix
@@ -1,11 +1,5 @@
 { config, pkgs, ... }:
 
-#
-# Notes about installation
-# ---
-# 1. chmod 0644 /var/lib/kubernetes/secrets/admin-key.pem
-# 
-
 let
   clusterAdminCert = config.services.kubernetes.lib.mkCert {
     name = "cluster-admin";
@@ -27,7 +21,38 @@ in
   systemd.services.kube-apiserver .serviceConfig.TimeoutSec = 10;
 
   systemd.services.containerd.after = pkgs.lib.mkForce [ "flannel.service" ];
-  networking.firewall.trustedInterfaces = [ "flannel.1" "mynet" ]; 
+  networking.firewall.trustedInterfaces = [ "flannel.1" "mynet" ];
+
+  environment.variables = {
+    KUBECONFIG="${clusterAdminKubeConfig}:$HOME/.kube/config";
+  };
+  home-manager.users.agondek.home.sessionVariables = {
+    KUBECONFIG="${clusterAdminKubeConfig}:$HOME/.kube/config";
+  };
+
+  users.groups = {
+    kubernet = {
+      members = [
+      "agondek"
+      "root"
+      "kubernet"
+      ];
+    };
+  };
+  systemd.services.kubernetes-admin-key-permissions = {
+    description = "Ensure k8s dev cluster admin key may easily accessed";
+    wantedBy = [ "multi-user.target" ];
+    requires = [ "kubernetes.slice" ];
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    script = ''
+      chown :kubernet /var/lib/kubernetes/secrets/cluster-admin.pem
+      chmod g+r /var/lib/kubernetes/secrets/cluster-admin.pem
+      chown :kubernet /var/lib/kubernetes/secrets/cluster-admin-key.pem
+      chmod g+r /var/lib/kubernetes/secrets/cluster-admin-key.pem
+    '';
+  };
 
   services.kubernetes = {
     roles = ["master" "node"];
@@ -46,12 +71,8 @@ in
     addons.dashboard.enable = false;
   };
   
-  # We need to apply this permissions, to use
-  # addon-manager in a bit 'haxy' way - 
-  # thanks to it, we can spin Ingress, Hostpath etc.
-  # on cluster start.
+  # Ensure addon-manager can literally do anything
   services.kubernetes.addonManager.bootstrapAddons = {
-    # TODO: Investigate and drop privileges not neeeded
     kube-addon-manager-allow-all = {
       kind = "ClusterRoleBinding";
       apiVersion = "rbac.authorization.k8s.io/v1";
@@ -71,17 +92,9 @@ in
       ];
     };
   };
- 
-  # TODO: Find more elegant solution
-  environment.variables = {
-    KUBECONFIG="${clusterAdminKubeConfig}:$HOME/.kube/config";
-  };
-  home-manager.users.agondek.home.sessionVariables = {
-    KUBECONFIG="${clusterAdminKubeConfig}:$HOME/.kube/config";
-  };
 
   imports = [
-    ./addons/admin.nix
+    ./addons/cluster-admin.nix
     ./addons/cluster-ingress.nix
     ./addons/dashboard.nix
     ./addons/hostpath-provisioner.nix
